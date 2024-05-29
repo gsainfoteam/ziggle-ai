d942b74402d2f0aa40e2e7c592779a41c382a96b
Shiwon Park <psw041115@gmail.com>
2024-05-29 11:56:19 +0000

    codespace setting and use request body

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from functools import lru_cache
 import config
 
 from fastapi import FastAPI, APIRouter
+from pydantic import BaseModel
 import mute_detection
 
+class DetectionRequest(BaseModel):
+    body: str 
 app=FastAPI()
 router = APIRouter()
 @lru_cache
@@ -16,8 +19,8 @@ async def root():
     return {"message": "Hello FastAPI"}
 
 @app.post("/mute_detection")
-def mute_check(body_query):
-    return mute_detection.mute_detection(body_query)
+def mute_check(body_query: DetectionRequest):
+    return mute_detection.mute_detection(body_query.body)
 # %%
 
 app.include_router(router, prefix="/api")
