2574ea32ff01a33c703dfbd55781f5d19454e303
inthree3 <42310616+inthree3@users.noreply.github.com>
2024-06-04 01:37:06 +0900

    feat: format the return notices

--- mute_detection.py
+++ mute_detection.py
@@ -5,6 +5,7 @@ from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import numpy as np
 import pandas as pd
+from pprint import pprint
 import embedding
 import os
 
@@ -30,23 +31,11 @@ collection=database["ziggle_content"]
 # # %%
 # print(len(df["body_embedded"][0]))
 # %%
-query_body="""
-[:cat: 인포팀(정보국) 2024년도 신규 부원 모집 :cat:]
-:dizzy: 인포팀 소개 페이지
-:dizzy: 인포팀 서류 접수 페이지
-
-서류 접수 시 'AMS가 처음이 아니신가요?' 버튼은 무시하셔도 됩니다!
-
-:dizzy: 문의사항 연락처
-제 카톡 개인 메세지로 문의해주세요!
-※ 학번을 아직 발급받지 못한 신입생 여러분들은 학번을 발급받고 신청해주세요!
-"""
-# %%
 def mute_detection(query_body):
     pipeline=[
         {
         "$vectorSearch":{
-            "index": "vector_index",
+            "index": "notice_vector_index",
             "path": "body_embedded",
             "queryVector": embedding.get_embedding(query_body),
             "numCandidates": 5,
@@ -55,8 +44,15 @@ def mute_detection(query_body):
         }, {
             "$project": {
                 "_id":0,
+                "id":1,
                 "title":1,
+                "views":1,
                 "body":1,
+                "currentDeadline":1,
+                "deadline":1,
+                "createdAt":1,
+                "author":1,
+                "langs":1,
                 "score": {"$meta": "vectorSearchScore"}
             }
         }
@@ -68,8 +64,52 @@ def mute_detection(query_body):
             return {"mute":True, "mute_content":{"title": result["title"], "body": result["body"]}}
         elif result["score"]<0.95:
             return {"mute":False}
+        
+def similar_notices(query_body):
+    pipeline=[
+        {
+        "$vectorSearch":{
+            "index": "notice_vector_index",
+            "path": "body_embedded",
+            "queryVector": embedding.get_embedding(query_body),
+            "numCandidates": 5,
+            "limit": 5,
+            }
+        }, {
+            "$project": {
+                "_id":0,
+                "id":1,
+                "title":1,
+                "views":1,
+                "body":1,
+                "currentDeadline":1,
+                "deadline":1,
+                "createdAt":1,
+                "author":1,
+                "langs":1,
+                "score": {"$meta": "vectorSearchScore"}
+            }
+        }
+    ]
+    results=collection.aggregate(pipeline)
+    return list(results)
 # %%
 if __name__=="__main__":
-    mute_result=mute_detection(query_body)
-    print(mute_result)
+    # TEST
+    query_body="""
+    [:cat: 인포팀(정보국) 2024년도 신규 부원 모집 :cat:]
+    :dizzy: 인포팀 소개 페이지
+    :dizzy: 인포팀 서류 접수 페이지
+
+    서류 접수 시 'AMS가 처음이 아니신가요?' 버튼은 무시하셔도 됩니다!
+
+    :dizzy: 문의사항 연락처
+    제 카톡 개인 메세지로 문의해주세요!
+    ※ 학번을 아직 발급받지 못한 신입생 여러분들은 학번을 발급받고 신청해주세요!
+    """
+    # mute_result=mute_detection(query_body)
+    # print(mute_result)
+    similar_result=similar_notices(query_body)
+    print(similar_result)
+
 # %%
