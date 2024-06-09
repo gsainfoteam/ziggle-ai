8208c2bed4acb65f01f48182a61b4f5fbf039b59
Shiwon Park <psw041115@gmail.com>
2024-06-09 14:26:33 +0000

    fix: using dict

--- mute_validation.py
+++ mute_validation.py
@@ -5,6 +5,8 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+from pydantic import BaseModel
+
 from dotenv import load_dotenv
 import os
 
@@ -13,10 +15,16 @@ load_dotenv(override=True)
 client = MongoClient(os.getenv('MONGODB_URI'), server_api=ServerApi('1'))
 client.admin.command("ping")
 
+class MuteEdgeCase(BaseModel):
+    source_body: str
+    result_body: str
+    similarity_score: float
+
 # %%
 database=client["ziggle_db"]
 collection=database["mute_edge_cases"]
 
 # %%
-def insert_edge_case(mute_failed_case):
-    collection.insert_one(mute_failed_case)
+def insert_edge_case(mute_failed_case: MuteEdgeCase):
+    inp = {"source_body": mute_failed_case.source_body, "result_body": mute_failed_case.result_body, "similarity_score": mute_failed_case.similarity_score}
+    collection.insert_one(inp)
